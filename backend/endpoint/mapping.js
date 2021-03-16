f53222aebfc9c8bc638ef4aca428a7e8a6c3563d
Moayyad Alfaris <moayyad.alfaris@gmail.com>
2021-03-16 19:52:10 -0400

    Adding config file for backend and modifying mapping.js

--- backend/endpoint/mapping.js
+++ backend/endpoint/mapping.js
@@ -1,3 +1,4 @@
+const fabricConfig = require('./config');
 let gateway; 
 
 async function init() {
@@ -8,20 +9,38 @@ async function init() {
         const path = require('path');
         const fs = require('fs');
     
-        const walletPath = path.join(process.cwd(), './wallet/Org1');
+        //const walletPath = path.join(process.cwd(), './wallet/Org1');
+        const walletPath = path.join(process.cwd(), fabricConfig.walletFolder);
+
         const wallet = await Wallets.newFileSystemWallet(walletPath);
         gateway = new Gateway();
-        const connectionProfilePath = path.resolve(__dirname, '../gateway', '1OrgLocalFabricOrg1GatewayConnection.json');
+        //const connectionProfilePath = path.resolve(__dirname, '../gateway', '1OrgLocalFabricOrg1GatewayConnection.json');
+
+        const connectionProfilePath = path.resolve(__dirname, fabricConfig.connProfileFolder, fabricConfig.connProfile);
+
         const connectionProfile = JSON.parse(fs.readFileSync(connectionProfilePath, 'utf8'));
+
+        /*
         const connectionOptions = {
             wallet, identity: 'Org1 Admin', discovery:
                 { enabled: true, asLocalhost: true }
         };
+        */
+
+        const connectionOptions = {
+            wallet, identity: fabricConfig.walletFile, discovery:
+                { enabled: true, asLocalhost: true }
+        };
+
         await gateway.connect(connectionProfile, connectionOptions);
+        
         // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
+        //const network = await gateway.getNetwork('mychannel');
+        const network = await gateway.getNetwork(fabricConfig.channelName);
+        
         // Get the contract from the network.
-        contract = network.getContract('EVChargerReg');
+        //contract = network.getContract('EVChargerReg');
+        contract = network.getContract(fabricConfig.contractName);
 
         console.log('---------------------------- RESULT from backend server --------')
 
